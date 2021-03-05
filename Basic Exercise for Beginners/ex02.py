3ebb44d1be6b00c5ec4119b294d7d62da0a5b54f
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-03-05 11:17:30 -0300

    Remove

--- Basic Exercise for Beginners/ex02.py
+++
@@ -1,9 +0,0 @@
-# Given a range of the first 10 numbers, Iterate from the start number to the end number, and In each iteration print the sum of the current number and previous number
-# Solution: https://github.com/JhonesBR
-
-for i in range(10):
-    if i == 0:
-        n = 0
-    else:
-        n = i-1
-    print(f"Current Number {i} Previous Number {n}  Sum: {i+n}")
