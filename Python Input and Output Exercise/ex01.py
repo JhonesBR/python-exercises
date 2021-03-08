e42898a3fe4356f7e9483571477b7e511bb4acf8
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-03-08 11:36:05 -0300

    ex 01

---
+++ Python Input and Output Exercise/ex01.py
@@ -0,0 +1,6 @@
+# Accept two numbers from the user and calculate multiplication
+# Solution: https://github.com/JhonesBR
+
+n1 = int(input("First number: "))
+n2 = int(input("Second number: "))
+print(f"{n1} * {n2} = {n1*n2}")
