f8cd133a77eb312fdd5037057d3806a0f97752f0
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-03-09 16:19:10 -0300

    ex02

---
+++ 2 - Python Input and Output Exercise/ex02.py
@@ -0,0 +1,4 @@
+# Question 2: Display “My Name Is James” as “My**Name**Is**James” using output formatting of a print() function
+# Solution: https://github.com/JhonesBR
+
+print("My", "Name", "Is", "James", sep="**")
