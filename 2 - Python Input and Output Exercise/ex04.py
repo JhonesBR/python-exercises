8bb8224f6ebcd226a0973d7924e0501478a17fe2
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-03-09 16:19:41 -0300

    ex04

---
+++ 2 - Python Input and Output Exercise/ex04.py
@@ -0,0 +1,5 @@
+# Display float number with 2 decimal places using print()
+# Solution: https://github.com/JhonesBR
+
+n = float(input("Insert number with more than 2 decimal places: "))
+print("%.2f" % n)
