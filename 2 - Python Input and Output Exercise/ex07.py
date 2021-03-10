f7b5169e01469783f74b49dff781fa42d923bca4
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-03-10 12:08:11 -0300

    ex07

---
+++ 2 - Python Input and Output Exercise/ex07.py
@@ -0,0 +1,5 @@
+# Accept any three string from one input() call
+# Solution: https://github.com/JhonesBR
+
+string1, string2, string3 = input("Insert 3 strings: ").split()
+print(string1, string2, string3)
