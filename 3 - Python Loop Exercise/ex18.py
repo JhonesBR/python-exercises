45ea3398dfad98184fda58d6698e41c4f297ce14
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-03-22 22:50:26 -0300

    ex18

---
+++ 3 - Python Loop Exercise/ex18.py
@@ -0,0 +1,20 @@
+# Print the following pattern
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+# Solution: https://github.com/JhonesBR
+
+for i in range(1, 6):
+    for j in range(i):
+        print("* ", end="")
+    print()
+for i in range(4, 0, -1):
+    for j in range(i):
+        print("* ", end="")
+    print()
