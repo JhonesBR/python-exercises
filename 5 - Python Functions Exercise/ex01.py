cad050e6d7cccacc6a27e99d4df1792ec0e2a7fb
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-04-09 16:22:13 -0300

    ex01

---
+++ 5 - Python Functions Exercise/ex01.py
@@ -0,0 +1,8 @@
+# Create a function that can accept two arguments name and age and print its value
+# Solution: https://github.com/JhonesBR
+
+def func(name, age):
+    print(f"Name: {name}\nAge: {age}")
+
+
+func("Jhones", "19")
