c2589560709ac1958157833614ab014c14608563
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-04 14:18:45 -0300

    ex01

--- 7 - Python List Exercise/ex01.py
+++ 7 - Python List Exercise/ex01.py
@@ -1,3 +1,9 @@
-# 
+# Reverse a given list in Python
 # Solution: https://github.com/JhonesBR
 
+def reverseList(L):
+    return L[::-1]
+
+
+aLsit = [100, 200, 300, 400, 500]
+print(reverseList(aLsit))
