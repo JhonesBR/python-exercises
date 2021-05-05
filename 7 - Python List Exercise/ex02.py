37f79c9baa3351c4dc84c7b035339b085c3d11bd
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-05 12:21:24 -0300

    ex02

--- 7 - Python List Exercise/ex02.py
+++ 7 - Python List Exercise/ex02.py
@@ -1,3 +1,13 @@
-# 
+# Concatenate two lists index-wise
 # Solution: https://github.com/JhonesBR
 
+def concatList(l1, l2):
+    concatedList = []
+    for i in range(0, len(l1)):
+        concatedList.append(l1[i]+l2[i])
+    return concatedList
+
+
+list1 = ["M", "na", "i", "Ke"]
+list2 = ["y", "me", "s", "lly"]
+print(concatList(list1, list2))
