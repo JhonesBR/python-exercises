79d25c751576c11bc734a38dbe55c5325fa3feaa
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-05 12:21:58 -0300

    ex03

--- 7 - Python List Exercise/ex03.py
+++ 7 - Python List Exercise/ex03.py
@@ -1,3 +1,11 @@
-# 
+# Given a Python list of numbers. Turn every item of a list into its square
 # Solution: https://github.com/JhonesBR
 
+def turnToSquare(L):
+    for i in range(len(L)):
+        L[i] = L[i]**2
+    return L
+
+
+aList = [1, 2, 3, 4, 5, 6, 7]
+print(turnToSquare(aList))
