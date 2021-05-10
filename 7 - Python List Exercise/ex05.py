2ddd40ba61b4967b0e605cc82d06ba06cb2f28a0
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-10 08:38:53 -0300

    ex05

--- 7 - Python List Exercise/ex05.py
+++ 7 - Python List Exercise/ex05.py
@@ -1,3 +1,12 @@
-# 
+# Given a two Python list. Iterate both lists simultaneously such that list1
+# should display item in original order and list2 in reverse order
 # Solution: https://github.com/JhonesBR
 
+def func(l1, l2):
+    for i in range(len(l1)):
+        print(l1[i], l2[len(l2)-1-i])
+
+
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+func(list1, list2)
