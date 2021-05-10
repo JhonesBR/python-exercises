0d378e5e9723b6a88a597b7ef0ef9bea41f28c6b
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-10 08:39:04 -0300

    ex06

--- 7 - Python List Exercise/ex06.py
+++ 7 - Python List Exercise/ex06.py
@@ -1,3 +1,9 @@
-# 
+# Remove empty strings from the list of strings
 # Solution: https://github.com/JhonesBR
 
+def removeEmpty(l):
+    return [i for i in l if i != ""]
+
+
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+print(removeEmpty(list1))
