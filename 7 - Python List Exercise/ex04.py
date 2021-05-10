76b9c6fcb3ca5e19146bb34084805d0d5c2abe6e
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-10 08:38:42 -0300

    ex04

--- 7 - Python List Exercise/ex04.py
+++ 7 - Python List Exercise/ex04.py
@@ -1,3 +1,17 @@
-# 
+# Concatenate two lists in the following order
+# list1 = ["Hello ", "take "]
+# list2 = ["Dear", "Sir"]
+# ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
 # Solution: https://github.com/JhonesBR
 
+def concatAll(l1, l2):
+    final = []
+    for x in l1:
+        for y in l2:
+            final.append(x+y)
+    return final
+
+
+list1 = ["Hello ", "take "]
+list2 = ["Dear", "Sir"]
+print(concatAll(list1, list2))
