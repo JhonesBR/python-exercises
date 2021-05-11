2b50e6b8a2ce1ea8031d6fb7d068cc4a1b45d556
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-11 08:46:07 -0300

    ex08

---
+++ 7 - Python List Exercise/ex08.py
@@ -0,0 +1,8 @@
+# Given a nested list extend it by adding the sub list ["h", "i", "j"] in such a way that it will look like the following list
+# list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+# Expected = ['a', 'b', ['c', ['d', 'e', ['f', 'g', 'h', 'i', 'j'], 'k'], 'l'], 'm', 'n']
+# Solution: https://github.com/JhonesBR
+
+list1 = ["a", "b", ["c", ["d", "e", ["f", "g"], "k"], "l"], "m", "n"]
+list1[2][1][2].extend(["h", "i", "j"])
+print(list1)
