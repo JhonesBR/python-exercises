ea913fdba591ccc13298c2240f7a4e92d3820945
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-24 22:50:47 -0300

    ex02

---
+++ 9- Python Set Exercise/ex02.py
@@ -0,0 +1,6 @@
+# Return a new set of identical items from a given two set
+# Solution: https://github.com/JhonesBR
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(set1.intersection(set2))
