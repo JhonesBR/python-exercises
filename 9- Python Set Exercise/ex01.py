2a8b1c8494bab6f039060525fa776e9266abd049
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-24 22:50:38 -0300

    ex01

---
+++ 9- Python Set Exercise/ex01.py
@@ -0,0 +1,8 @@
+# Add a list of elements to a given set
+# Solution: https://github.com/JhonesBR
+
+sampleSet = {"Yellow", "Orange", "Black"}
+sampleList = ["Blue", "Green", "Red"]
+
+sampleSet.update(sampleList)
+print(sampleSet)
