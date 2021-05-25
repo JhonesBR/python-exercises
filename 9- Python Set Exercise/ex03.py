7f9570b1f8b4dd080603869b430151e1799fe60f
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-24 22:50:57 -0300

    ex03

---
+++ 9- Python Set Exercise/ex03.py
@@ -0,0 +1,6 @@
+# Returns a new set with all items from both sets by removing duplicates
+# Solution: https://github.com/JhonesBR
+
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+print(set1.union(set2))
