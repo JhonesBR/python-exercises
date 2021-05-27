8a81fd31462b71c0732c6b035c0c07bb03ab2e60
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-27 10:44:03 -0300

    ex05

--- 9- Python Set Exercise/ex05.py
+++ 9- Python Set Exercise/ex05.py
@@ -1,3 +1,7 @@
-# 
+# Remove items 10, 20, 30 from the following set at once
 # Solution: https://github.com/JhonesBR
 
+set1 = {10, 20, 30, 40, 50}
+
+set1.difference_update({10, 20, 30})
+print(set1)
