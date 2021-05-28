e66ed73791e36dab79f9a8f2bbad095488ab548f
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-05-28 00:38:21 -0300

    ex06

--- 9- Python Set Exercise/ex06.py
+++ 9- Python Set Exercise/ex06.py
@@ -1,3 +1,8 @@
-# 
+# Return a set of all elements in either A or B, but not both
 # Solution: https://github.com/JhonesBR
 
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+set3 = set1.symmetric_difference(set2)
+print(set3)
