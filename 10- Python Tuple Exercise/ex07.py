c6ff3f14af9363c26c712d8c537edd2b1e936c8a
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-07 09:41:08 -0300

    ex07

--- 10- Python Tuple Exercise/ex07.py
+++ 10- Python Tuple Exercise/ex07.py
@@ -1,3 +1,7 @@
-# 
+# Modify the first item (22) of a list inside a following tuple to 222
 # Solution: https://github.com/JhonesBR
 
+tuple1 = (11, [22, 33], 44, 55)
+tuple1[1][0] = 222
+
+print(tuple1)
