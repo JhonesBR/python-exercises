8128fada357f0de6b48a436f91bf8d0d596c1386
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-11 10:10:50 -0300

    ex10

--- 10- Python Tuple Exercise/ex10.py
+++ 10- Python Tuple Exercise/ex10.py
@@ -1,3 +1,13 @@
-# 
+# Check if all items in the following tuple are the same
 # Solution: https://github.com/JhonesBR
 
+def allEqual(t):
+    first = t[0]
+    for i in t:
+        if i != first:
+            return False
+    return True
+
+
+tuple1 = (45, 45, 45, 45)
+print(allEqual(tuple1))
