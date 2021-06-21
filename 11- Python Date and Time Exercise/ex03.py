f8a8c134b5b8eab5e619a82ef128c9937fc4c2a2
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-21 10:16:16 -0300

    ex03

--- 11- Python Date and Time Exercise/ex03.py
+++ 11- Python Date and Time Exercise/ex03.py
@@ -1,3 +1,11 @@
-# 
+# Subtract a week (7 days)  from a given date in Python
 # Solution: https://github.com/JhonesBR
 
+from datetime import datetime, timedelta
+
+
+given_date = datetime(2020, 2, 25)
+final_date = given_date - timedelta(days=7)
+
+print(f"given_data: {given_date}")
+print(f"final_data: {final_date}")
