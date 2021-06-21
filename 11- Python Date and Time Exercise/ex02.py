8090d28235b091b460be693661f38c3587a58923
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-21 10:16:08 -0300

    ex02

--- 11- Python Date and Time Exercise/ex02.py
+++ 11- Python Date and Time Exercise/ex02.py
@@ -1,3 +1,9 @@
-# 
+# Convert string into a datetime object
 # Solution: https://github.com/JhonesBR
 
+from datetime import datetime
+
+
+string = "Feb 04 2020  9:15AM"
+data = datetime.strptime(string, '%b %d %Y %I:%M%p')
+print(data)
