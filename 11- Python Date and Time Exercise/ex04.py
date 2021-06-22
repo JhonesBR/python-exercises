822a18d39623c84f8090cae0cba6e11f77da4a8e
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-22 14:25:06 -0300

    ex04

--- 11- Python Date and Time Exercise/ex04.py
+++ 11- Python Date and Time Exercise/ex04.py
@@ -1,3 +1,9 @@
-# 
+# Print a date in a the following format
+# Day_name  Day_number  Month_name  Year
 # Solution: https://github.com/JhonesBR
 
+from datetime import datetime
+
+
+given_date = datetime(2020, 2, 25)
+print(given_date.strftime('%A %d %B %Y'))
