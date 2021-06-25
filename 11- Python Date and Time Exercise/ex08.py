f7041609e7c27324cac985dddf6a265616b42cb5
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-24 23:37:33 -0300

    ex08

--- 11- Python Date and Time Exercise/ex08.py
+++ 11- Python Date and Time Exercise/ex08.py
@@ -1,3 +1,9 @@
-# 
+# Convert the following datetime into a string
 # Solution: https://github.com/JhonesBR
 
+from datetime import datetime
+
+
+given_date = datetime(2020, 2, 25)
+string = given_date.strftime("%Y-%m-%d %H:%M:%S")
+print(string)
