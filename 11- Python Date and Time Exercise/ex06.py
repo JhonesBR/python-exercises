f2eb1376b3472bb77e0577229dd689b214abb956
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-24 23:37:12 -0300

    ex06

--- 11- Python Date and Time Exercise/ex06.py
+++ 11- Python Date and Time Exercise/ex06.py
@@ -1,3 +1,11 @@
-# 
+# Add a week (7 days) and 12 hours to a given date
 # Solution: https://github.com/JhonesBR
 
+from datetime import datetime, timedelta
+
+
+given_date = datetime(2020, 3, 22, 10, 0, 0)
+final_date = given_date + timedelta(days=7, hours=12)
+
+print(f"given_data: {given_date}")
+print(f"final_data: {final_date}")
