c3efcaaca6ae47bcd42eb2d3bf0d64c5ce9f2215
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-25 15:39:40 -0300

    ex10

--- 11- Python Date and Time Exercise/ex10.py
+++ 11- Python Date and Time Exercise/ex10.py
@@ -1,3 +1,9 @@
-# 
+# Calculate number of days between two given dates
 # Solution: https://github.com/JhonesBR
 
+from datetime import datetime, timedelta
+
+
+date_1 = datetime(2020, 2, 25)
+date_2 = datetime(2020, 9, 17)
+print(f"{(date_2-date_1).days} days")
