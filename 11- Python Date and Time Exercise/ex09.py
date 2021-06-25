a0fba304f05d161f893fd6bba91b958a5095a324
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-06-24 23:37:43 -0300

    ex09

--- 11- Python Date and Time Exercise/ex09.py
+++ 11- Python Date and Time Exercise/ex09.py
@@ -1,3 +1,12 @@
-# 
+# Calculate the date 4 months from the current date
 # Solution: https://github.com/JhonesBR
 
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+
+given_date = given_date = datetime(2020, 2, 25).date()
+final_date = given_date + relativedelta(months=4)
+
+print(f"given_data: {given_date}")
+print(f"final_data: {final_date}")
