5a01b68cce10ef6d92532669433378f32b7a02a1
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-07-14 21:33:37 -0300

    ex03

--- 12- Python OOP Exercise/ex03.py
+++ 12- Python OOP Exercise/ex03.py
@@ -1,3 +1,11 @@
-# 
+# Create a child class Bus that will inherit all of the variables and methods of the Vehicle class
+# Given:
+# class Vehicle:
+
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+# Create a Bus object that will inherit all of the variables and methods of the Vehicle class and display it.
 # Solution: https://github.com/JhonesBR
 
