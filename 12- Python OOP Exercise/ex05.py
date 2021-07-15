3ccf00fe0d431b58f8a24748bf2a9339e5382055
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-07-15 04:09:00 -0300

    ex05

--- 12- Python OOP Exercise/ex05.py
+++ 12- Python OOP Exercise/ex05.py
@@ -1,3 +1,17 @@
-# 
+# Define property that should have the same value for every class instance
+# Define a class attribute”color” with a default value white. I.e., Every Vehicle should be white.
+# Use the following code for this exercise.
+# class Vehicle:
+
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+
+# class Bus(Vehicle):
+#     pass
+
+# class Car(Vehicle):
+#     pass
 # Solution: https://github.com/JhonesBR
 
