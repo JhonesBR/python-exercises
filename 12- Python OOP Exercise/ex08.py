1a069164582c7d9df95c8f72d8cba2782b2b42a9
Jhones <41956039+JhonesBR@users.noreply.github.com>
2021-07-23 00:24:32 -0300

    ex08

--- 12- Python OOP Exercise/ex08.py
+++ 12- Python OOP Exercise/ex08.py
@@ -1,3 +1,14 @@
-# 
+# Determine if School_bus is also an instance of the Vehicle class
+# Given:
+# class Vehicle:
+#     def __init__(self, name, mileage, capacity):
+#         self.name = name
+#         self.mileage = mileage
+#         self.capacity = capacity
+
+# class Bus(Vehicle):
+#     pass
+
+# School_bus = Bus("School Volvo", 12, 50)
 # Solution: https://github.com/JhonesBR
 
